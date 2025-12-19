77d758529f3f0a3dfe0fe76e10e85dcf8ce04edd
Notekill <104700152+Notekill@users.noreply.github.com>
2025-12-19 14:31:12 +0100

    Update script.js

--- public/script.js
+++ public/script.js
@@ -11,7 +11,7 @@ async function iniciarDescarga() {
 
     try {
         console.log("Enviando petición para:", url);
-        const infoRes = await fetch('./api/info', {
+        const infoRes = await fetch('api/info', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ url: url })
@@ -35,4 +35,5 @@ async function iniciarDescarga() {
         statusMessage.className = "status-bar error";
     }
 }
+
 
