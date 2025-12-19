f05d06f6b99ad6c9b34fcbb16be8bd2bb36b050a
johelvis <gratisw40@gmail.com>
2025-12-19 00:41:00 -0400

    Delete kill_all.bat

--- kill_all.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Cerrando todo por la fuerza...
-taskkill /F /IM node.exe /T
-taskkill /F /IM electron.exe /T
-echo.
-echo !Listo! Todo cerrado. Ya puedes intentar abrir la app de nuevo.
-pause
